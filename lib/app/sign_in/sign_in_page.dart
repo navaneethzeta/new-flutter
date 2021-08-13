e9ae6d0ca8d6edfe4fa4689dcb8ac45ad238c1c9
m s dhoni <navaneethpa13@gmail.com>
2021-08-13 09:53:08 +0530

    2

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:new_flutter/app/sign_in/sign_in_button.dart';
 import 'package:new_flutter/app/sign_in/social_sign_in_button.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,7 @@ class SignInPage extends StatelessWidget {
   SignInPage({required this.onSignIn});
   final Function(User)onSignIn;
   Future<void> _signInAnonymously() async {
+    await Firebase.initializeApp();
    try{
   
     final authResult = await FirebaseAuth.instance.signInAnonymously();
